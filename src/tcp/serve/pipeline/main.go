0653ea3aeb6f9a7c03a705b392af4c4f6dbc2570
ryutaroM <arcticmonkey326@gmail.com>
2025-04-07 23:21:07 +0900

    add pipelining

---
+++ src/tcp/serve/pipeline/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+func writeToConn(sessionResponses chan chan *http.Response, conn net.Conn) {
+	defer conn.Close()
+	for sessionResponse := range sessionResponses {
+		response := <-sessionResponse
+		response.Write(conn)
+		close(sessionResponse)
+	}
+}
+
+func handleRequest(request *http.Request, resultReceiver chan *http.Response) {
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	content := "Hello World\n"
+	response := &http.Response{
+		StatusCode:    http.StatusOK,
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(content)),
+		Body:          io.NopCloser(strings.NewReader(content)),
+	}
+	resultReceiver <- response
+}
+
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	sessionResponses := make(chan chan *http.Response, 50)
+	defer close(sessionResponses)
+	go writeToConn(sessionResponses, conn)
+	reader := bufio.NewReader(conn)
+	for {
+		conn.SetDeadline(time.Now().Add(5 * time.Second))
+		request, err := http.ReadRequest(reader)
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		sessionResponse := make(chan *http.Response)
+		sessionResponses <- sessionResponse
+		go handleRequest(request, sessionResponse)
+	}
+}
