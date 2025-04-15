e298c75c2bc50c8ba2f521ff79eab78a231192b2
ryutaroM <arcticmonkey326@gmail.com>
2025-04-15 23:38:01 +0900

    add unix domain sock http client and server

---
+++ src/unixdomain/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	path := filepath.Join(os.TempDir(), "unixdomainsocket-sample")
+	os.Remove(path)
+	listener, err := net.Listen("unix", path)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	fmt.Println("Serve is running at" + path)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+			response := http.Response{
+				StatusCode: http.StatusOK,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body: io.NopCloser(
+					strings.NewReader("Hello, world!\n"),
+				),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
+}
