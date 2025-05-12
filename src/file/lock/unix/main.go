35f99d9ec448687ec0909c739f7bb1ab570913d8
ryutaroM <arcticmonkey326@gmail.com>
2025-05-13 00:16:42 +0900

    add file lock and mmap

---
+++ src/file/lock/unix/main.go
@@ -0,0 +1,29 @@
+//go:build unix
+
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+type lockType int16
+
+const (
+	readLock  lockType = syscall.LOCK_SH
+	writeLock lockType = syscall.LOCK_EX
+)
+
+func lock(f os.File, lt lockType) (err error) {
+	for {
+		err = syscall.Flock(int(f.Fd()), int(lt))
+		if err != syscall.EINTR {
+			break
+		}
+	}
+	return err
+}
+
+func unlock(f os.File) (err error) {
+	return lock(f, syscall.LOCK_UN)
+}
