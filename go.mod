4bcb03d3964d5dd26ea6db6e0697cb4432db20b1
ryutaroM <arcticmonkey326@gmail.com>
2025-06-03 23:42:24 +0900

    add graceful restart

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.23.7
 
 require (
 	github.com/edsrzf/mmap-go v1.2.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lestrrat-go/server-starter v0.0.0-20210101230921-50cd1900b5bc // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
 )
---
+++ src/serverstarter/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/lestrrat-go/server-starter/listener"
+)
+
+func main() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM)
+
+	listeners, err := listener.ListenAll()
+	if err != nil {
+		panic(err)
+	}
+
+	server := http.Server{
+		Handler: http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "server pid: %d %v\n", os.Getpid(), os.Environ())
+			},
+		),
+	}
+
+	go server.Serve(listeners[0])
+
+	<-signals
+	server.Shutdown(context.Background())
+}
